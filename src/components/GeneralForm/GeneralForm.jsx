5e1378ba7a998090f00898843f9c10e4416bac87
incandesc3nce <62383010+incandesc3nce@users.noreply.github.com>
2024-07-18 14:34:28 +0700

    feat: add heading

--- src/components/GeneralForm/GeneralForm.jsx
+++ src/components/GeneralForm/GeneralForm.jsx
@@ -2,27 +2,30 @@ import "../styles/form.css";
 
 export function GeneralForm() {
   return (
-    <form className="general-form">
-      <label htmlFor="name">Full Name</label>
-      <input
-        type="text"
-        name="name"
-        value="Bruce Wayne"
-        placeholder="Enter your full name"
-      />
-      <label htmlFor="email">Email</label>
-      <input
-        type="text"
-        name="email"
-        value="ceo@waynecorp.com"
-        placeholder="Enter your email"
-      />
-      <label htmlFor="phone">Phone</label>
-      <input
-        type="text"
-        value="(123)456-7890"
-        placeholder="Enter your phone number"
-      />
-    </form>
+    <>
+      <h2>Personal Details</h2>
+      <form className="general-form">
+        <label htmlFor="name">Full Name</label>
+        <input
+          type="text"
+          name="name"
+          value="Bruce Wayne"
+          placeholder="Enter your full name"
+        />
+        <label htmlFor="email">Email</label>
+        <input
+          type="text"
+          name="email"
+          value="ceo@waynecorp.com"
+          placeholder="Enter your email"
+        />
+        <label htmlFor="phone">Phone</label>
+        <input
+          type="text"
+          value="(123)456-7890"
+          placeholder="Enter your phone number"
+        />
+      </form>
+    </>
   );
 }
