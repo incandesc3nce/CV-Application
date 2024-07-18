fd8f3789ab06db9062e86878d7b1bcd85be0d347
incandesc3nce <62383010+incandesc3nce@users.noreply.github.com>
2024-07-18 18:14:48 +0700

    feat: pass general info state variables to form and cv

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,31 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import { GeneralForm } from "./components/GeneralForm/GeneralForm";
+import { EducationDropdown } from "./components/DropdownMenu/EducationDropdown";
+import { ExperienceDropdown } from "./components/DropdownMenu/ExperienceDropdown";
+import { CV } from "./components/CV/CV";
 
 function App() {
+  const [name, setName] = useState('Bruce Wayne');
+  const [email, setEmail] = useState('ceo@waynecorp.com');
+  const [phone, setPhone] = useState('(123)456-7890');
+  const [location, setLocation] = useState('Gotham City, US');
+  const [experience, setExperience] = useState([]);
+
   return (
     <>
-      
+    <div className="forms-wrapper">
+      <section className="general-form">
+        <GeneralForm name={{name, setName}} email={{email, setEmail}} phone={{phone, setPhone}} location={{location, setLocation}}/>
+      </section>
+      <EducationDropdown />
+      <ExperienceDropdown />
+    </div>
+    <div className="cv-wrapper">
+      <CV name={name} email={email} phone={phone} location={location} experience={experience}/>
+    </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
