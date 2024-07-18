5f5029a49d06f4b02d48a14b524232324c97235a
incandesc3nce <62383010+incandesc3nce@users.noreply.github.com>
2024-07-18 12:11:33 +0700

    feat: add placeholders

--- src/components/ExperienceForm/ExperienceForm.jsx
+++ src/components/ExperienceForm/ExperienceForm.jsx
@@ -5,24 +5,24 @@ export function ExperienceForm() {
     <form className="experience-form">
       <div className='input-field'>
         <label htmlFor="company">Company Name</label>
-        <input type="text" name="company" />
+        <input type="text" name="company" placeholder='Arasaka Corporation' required/>
       </div>
       <div className='input-field'>
         <label htmlFor="position">Position Title</label>
-        <input type="text" name="position" />
+        <input type="text" name="position" placeholder='Software Engineer' required/>
       </div>
       <div className='date-form'>
         <div className='date-input'>
           <label htmlFor="startDate">Start Date</label>
-          <input type="date" name="startDate" />
+          <input type="date" name="startDate" required/>
         </div>
         <div className='date-input'>
           <label htmlFor="endDate">End Date</label>
-          <input type="date" name="endDate" />
+          <input type="date" name="endDate" required/>
         </div>
       </div>
       <div className='input-field'>
-        <label htmlFor="description">Description</label>
+        <label htmlFor="description">Description <i>(optional)</i></label>
         <textarea name="description" />
       </div>
     </form>
--- src/components/GeneralForm/GeneralForm.jsx
+++ src/components/GeneralForm/GeneralForm.jsx
@@ -4,11 +4,11 @@ export function GeneralForm() {
   return (
     <form className="general-form">
       <label htmlFor="name">Full Name</label>
-      <input type="text" name="name" value="Bruce Wayne" />
+      <input type="text" name="name" value="Bruce Wayne" placeholder="Enter your full name"/>
       <label htmlFor="email">Email</label>
-      <input type="text" name="email" value="ceo@waynecorp.com" />
+      <input type="text" name="email" value="ceo@waynecorp.com" placeholder="Enter your email"/>
       <label htmlFor="phone">Phone</label>
-      <input type="text" value="(123)456-7890" />
+      <input type="text" value="(123)456-7890" placeholder="Enter your email"/>
     </form>
   );
 }
