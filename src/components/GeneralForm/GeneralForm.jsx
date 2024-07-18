fb60bdf07faa99d1c14cc1a9a9d2a44a5e92cbb0
incandesc3nce <62383010+incandesc3nce@users.noreply.github.com>
2024-07-18 16:47:15 +0700

    fix: resolve issues with ambigious labels

--- src/components/GeneralForm/GeneralForm.jsx
+++ src/components/GeneralForm/GeneralForm.jsx
@@ -9,6 +9,7 @@ export function GeneralForm() {
         <input
           type="text"
           name="name"
+          id="name"
           value="Bruce Wayne"
           placeholder="Enter your full name"
         />
@@ -16,18 +17,23 @@ export function GeneralForm() {
         <input
           type="text"
           name="email"
+          id="email"
           value="ceo@waynecorp.com"
           placeholder="Enter your email"
         />
         <label htmlFor="phone">Phone</label>
         <input
           type="text"
+          name="phone"
+          id="phone"
           value="(123)456-7890"
           placeholder="Enter your phone number"
         />
         <label htmlFor="gen_location">Location</label>
         <input
           type="text"
+          name="gen_location"
+          id="gen_location"
           value="Gotham City, US"
           placeholder="City, Country"
         />
