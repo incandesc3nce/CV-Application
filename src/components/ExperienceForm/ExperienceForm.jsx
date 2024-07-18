d3aac218e465fc84cac7dd35f600e5723d74c541
incandesc3nce <62383010+incandesc3nce@users.noreply.github.com>
2024-07-18 12:14:59 +0700

    style: apply Prettier

--- src/components/ExperienceForm/ExperienceForm.jsx
+++ src/components/ExperienceForm/ExperienceForm.jsx
@@ -1,28 +1,40 @@
-import './ExperienceForm.css';
+import "./ExperienceForm.css";
 
 export function ExperienceForm() {
   return (
     <form className="experience-form">
-      <div className='input-field'>
+      <div className="input-field">
         <label htmlFor="company">Company Name</label>
-        <input type="text" name="company" placeholder='Arasaka Corporation' required/>
+        <input
+          type="text"
+          name="company"
+          placeholder="Arasaka Corporation"
+          required
+        />
       </div>
-      <div className='input-field'>
+      <div className="input-field">
         <label htmlFor="position">Position Title</label>
-        <input type="text" name="position" placeholder='Software Engineer' required/>
+        <input
+          type="text"
+          name="position"
+          placeholder="Software Engineer"
+          required
+        />
       </div>
-      <div className='date-form'>
-        <div className='date-input'>
+      <div className="date-form">
+        <div className="date-input">
           <label htmlFor="startDate">Start Date</label>
-          <input type="date" name="startDate" required/>
+          <input type="date" name="startDate" required />
         </div>
-        <div className='date-input'>
+        <div className="date-input">
           <label htmlFor="endDate">End Date</label>
-          <input type="date" name="endDate" required/>
+          <input type="date" name="endDate" required />
         </div>
       </div>
-      <div className='input-field'>
-        <label htmlFor="description">Description <i>(optional)</i></label>
+      <div className="input-field">
+        <label htmlFor="description">
+          Description <i>(optional)</i>
+        </label>
         <textarea name="description" />
       </div>
     </form>
--- src/components/GeneralForm/GeneralForm.jsx
+++ src/components/GeneralForm/GeneralForm.jsx
@@ -4,11 +4,21 @@ export function GeneralForm() {
   return (
     <form className="general-form">
       <label htmlFor="name">Full Name</label>
-      <input type="text" name="name" value="Bruce Wayne" placeholder="Enter your full name"/>
+      <input
+        type="text"
+        name="name"
+        value="Bruce Wayne"
+        placeholder="Enter your full name"
+      />
       <label htmlFor="email">Email</label>
-      <input type="text" name="email" value="ceo@waynecorp.com" placeholder="Enter your email"/>
+      <input
+        type="text"
+        name="email"
+        value="ceo@waynecorp.com"
+        placeholder="Enter your email"
+      />
       <label htmlFor="phone">Phone</label>
-      <input type="text" value="(123)456-7890" placeholder="Enter your email"/>
+      <input type="text" value="(123)456-7890" placeholder="Enter your email" />
     </form>
   );
 }
